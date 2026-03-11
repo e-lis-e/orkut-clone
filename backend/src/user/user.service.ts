31e2098c1caa99ece41792eaa260fd2cea940a36
e-lis-e <elisenovaes10@gmail.com>
2026-03-11 11:34:10 -0300

    user service

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async create(data: UserDto) {
+    const user = await this.prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
+}
