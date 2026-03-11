6aea94085d4c34a29d0664a1ccf2087b8108e7b8
e-lis-e <elisenovaes10@gmail.com>
2026-03-11 11:34:19 -0300

    user controller

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  //Put,
+  //Delete,
+  Param,
+  Body,
+  //HttpCode,
+  //HttpStatus,
+} from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  //GET /user
+  @Get()
+  findAll() {
+    return [];
+  }
+  //GET /user/:id
+  @Get()
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+  //POST /user
+  @Post()
+  create(@Body() data: UserDto) {
+    return this.userService.create(data);
+  }
+  //PUT /user/:id
+  //   @Put(':id')
+  //   update(@Param('id') id: string, @Body() data: UserDto) {
+  //     return {
+  //       //   id,
+  //       //   updateUserDto,
+  //     };
+  //   }
+  //DELETE /user/:id
+  //   @Delete(':id')
+  //   @HttpCode(HttpStatus.NO_CONTENT)
+  //   remove(@Param('id') id: string) {}
+}
