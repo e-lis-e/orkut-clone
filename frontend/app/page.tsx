5bdf410a60878408563bd2a757910ad671c899bb
e-lis-e <elisenovaes10@gmail.com>
2026-03-12 16:47:49 -0300

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
       <div className="main-block">
-        
+        <h1>Tela Inicial</h1>
       </div>
     </div>
   );
