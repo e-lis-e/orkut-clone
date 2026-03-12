bb124a3e5aaf14f155206d541499e0444b8e6ba3
e-lis-e <elisenovaes10@gmail.com>
2026-03-12 16:34:06 -0300

    atualização da página de registro

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,16 +1,36 @@
-import Link from "next/link";
+import Image from "next/image";
+import ButtonComponent from "./button";
 
 export default function Register() {
     return (
         <div>
-            <h1>
-                Bem vindo ao [...]
-            </h1>
-            <a> Nome </a>
-            <a> Email </a>
-            <a> Senha </a>
-            <h3>já possui conta? <Link href="../login">Fazer login</Link></h3>
+            <div className="main-block">
+                <div className="container-block">
+                    <p />
+                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Logo_ORKUT.svg/500px-Logo_ORKUT.svg.png" alt="logo orkut" width={150} height={50} />
+                    <p />
+                    <h2> Conecta-se aos seus amigos e familiares usando recados e mensagens instantâneas</h2>
+                    <h2>Conheça novas pessoas através de amigos de seus amigos e comunidades</h2>
+                    <h2>Compartilhe seus videos, fotos e paixões em um só lugar</h2>
 
+                </div>
+
+                <div className="container-block">
+
+                    <h1>
+                        Crie a sua conta do orkut
+                    </h1>
+                    <h2>Nome: </h2><input type="name" name="" placeholder="nome" id="name"></input>
+                    <h2>E-mail: </h2><input type="email" name="" placeholder="E-mail" id="email"></input>
+                    <h2>Senha: </h2><input type="password" placeholder="Senha" id="password" />
+                    <input type="checkbox" name="info-save" />
+                    <label>Salvar as minhas informações neste computador. </label>
+                    <p></p>
+                    <ButtonComponent />
+
+                </div>
+
+            </div>
         </div>
     )
 }
