20d778b130491ac56aec905fa2ac060cc268a6b0
e-lis-e <elisenovaes10@gmail.com>
2026-03-12 16:16:26 -0300

    alteração da página de login

---
+++ frontend/app/login/button.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
+export default function ButtonComponent() {
+  const router = useRouter();
+
+  return (
+    <button onClick={() => router.push("/")}>
+      Login
+    </button>
+  );
+}
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
+import ButtonComponent from "./button";
 
-export default function login() {
+export default function Login() {
     return (
         <div className="main-block">
-            <div className="logo-block">
+            <div className="container-block">
+                <p/>
+                <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Logo_ORKUT.svg/500px-Logo_ORKUT.svg.png" alt="logo orkut" width={150} height={50} />
+                <p/>
                 <h2> Conecta-se aos seus amigos e familiares usando recados e mensagens instantâneas</h2>
                 <h2>Conheça novas pessoas através de amigos de seus amigos e comunidades</h2>
                 <h2>Compartilhe seus videos, fotos e paixões em um só lugar</h2>
@@ -16,15 +21,17 @@ export default function login() {
                     Acesse o orkut.br com a sua conta
                 </h1>
                 <h2>E-mail: </h2><input type="email" name="" placeholder="E-mail ou número de celular" id="email"></input>
-                <h2>Senha: </h2><input type="senha" name="" placeholder="Senha" id="password"></input>
+                <h2>Senha: </h2><input type="password" placeholder="Senha" id="password" />
                 <input type="checkbox" name="info-save" />
                 <label>Salvar as minhas informações neste computador. </label>
                 <p></p>
-                <Link href="../recoverPassword">Não consegue acessar a sua conta?</Link>
+                <ButtonComponent/>
+                <p></p>
+                <Link href="/recoverPassword">Não consegue acessar a sua conta?</Link>
 
                 <div>
                     <h2>Ainda não é membro?</h2>
-                    <Link href="../register">ENTRAR JÁ</Link>
+                    <Link href="/register">ENTRAR JÁ</Link>
                 </div>
 
             </div>
